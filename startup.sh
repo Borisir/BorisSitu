9cff51a1d3f2b012c46db74e4209e51c41ca366b
Boris Rengel <boris.rengel@unl.edu.ec>
2026-05-26 22:54:28 -0500

    fix: corregir CRLF en startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-cd /home/site/wwwroot/src/ProyectoSITU
-pip install -r /home/site/wwwroot/requirements.txt
-gunicorn --bind=0.0.0.0:8000 --timeout 600 ProyectoSITU.wsgi:application
+#!/bin/bashcd /home/site/wwwroot/src/ProyectoSITUpip install -r /home/site/wwwroot/requirements.txtgunicorn --bind=0.0.0.0:8000 --timeout 600 ProyectoSITU.wsgi:application
