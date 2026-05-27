d0bb92b23151565b3ace0c3ce7ea04f3272068f9
Boris Rengel <boris.rengel@unl.edu.ec>
2026-05-26 23:26:32 -0500

    fix: usar python3 en startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/site/wwwroot/src/ProyectoSITU
 pip install -r /home/site/wwwroot/requirements.txt
-python manage.py migrate
+python3 manage.py migrate
 gunicorn --bind=0.0.0.0:8000 --timeout 600 ProyectoSITU.wsgi:application
